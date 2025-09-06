3e3b4869d17c073a0252224f8dc537afdde6b2bb
ProfNpc <professornpc@gmail.com>
2025-09-05 22:07:38 -0300

    Versão inicial

---
+++ src/main/java/lab/crud/api/ApiApplication.java
@@ -0,0 +1,25 @@
+package lab.crud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+	
+	@GetMapping("/eco/{msg}")
+	public String eco(
+			@PathVariable("msg") String msg) {
+		
+		return msg;
+		
+	}
+}
+
