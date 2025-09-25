8f4c29335a3f1c1959615d69db5c477eb53759fa
Cesar Campos <evandro910@hotmail.com>
2025-09-24 22:47:19 -0300

    Criado método obter todos ()

--- src/main/java/lab/crud/api/controller/ProdutoController.java
+++ src/main/java/lab/crud/api/controller/ProdutoController.java
@@ -1,7 +1,12 @@
 package lab.crud.api.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +16,9 @@ import lab.crud.api.model.Produto;
 @RestController
 public class ProdutoController {
 
-	
+private List<Produto> listaProdutos = new ArrayList<Produto>();	
+private static int proxId = 1;
+
 	//curl -X POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	
 	//@RequestMapping(method = RequestMethod.POST, path = "/produto")
@@ -19,12 +26,20 @@ public class ProdutoController {
 	public ResponseEntity<Produto> novo(
 			@RequestBody Produto produto) {
 		
-		System.out.println("nome:" + produto.getNome());
-		System.out.println("mês:" + produto.getDataCriacao().getMonthValue());
+		produto.setId(proxId++);
+		produto.setDataCriacao(LocalDate.now());
+	
+		System.out.println(produto.toString());
+		this.listaProdutos.add(produto);
 		
 		return ResponseEntity
-				.status(HttpStatus.NOT_FOUND)
+				.status(HttpStatus.CREATED)
 				.body(produto);
 	}
-	
+	@GetMapping("/produtos")
+	public ResponseEntity<List<Produto>> obterTodos(){
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(this.listaProdutos);
+	}
 }
